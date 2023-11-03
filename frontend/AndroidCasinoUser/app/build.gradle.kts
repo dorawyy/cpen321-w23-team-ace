9c115b46714b11eb274423fbaad55a3b7f39fb6e
David123-web <69841889+David123-web@users.noreply.github.com>
2023-11-03 01:41:37 -0700

    Changed the URL of socket io

--- frontend/AndroidCasinoUser/app/build.gradle.kts
+++ frontend/AndroidCasinoUser/app/build.gradle.kts
@@ -10,11 +10,13 @@ android {
         applicationId = "com.davidzhang.androidcasinouser"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
+
 
     buildTypes {
         release {
@@ -35,6 +37,8 @@ android {
         exclude ("META-INF/LICENSE.md")
         exclude ("META-INF/NOTICE.md")
     }
+
+
 }
 
 dependencies {
