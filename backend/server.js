939bb6ef9baa2f8c8bc9234f64ac37691cd784af
David123-web <69841889+David123-web@users.noreply.github.com>
2023-11-30 15:22:09 -0800

    delete path

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const socketIo = require('socket.io');
 // const fs = require('fs');
 const cors = require('cors');
-const path = require('path');
+//const path = require('path');
 
 // server configuration
 const app = express();
