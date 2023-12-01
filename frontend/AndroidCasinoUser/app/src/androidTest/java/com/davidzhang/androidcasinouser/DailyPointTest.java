95693f939417319c9fb8bf601244152aa2f6c858
AReiter13 <ajreiter13@gmail.com>
2023-11-30 18:26:57 -0800

    Finished Codacy

--- frontend/AndroidCasinoUser/app/src/androidTest/java/com/davidzhang/androidcasinouser/DailyPointTest.java
+++ frontend/AndroidCasinoUser/app/src/androidTest/java/com/davidzhang/androidcasinouser/DailyPointTest.java
@@ -16,7 +16,6 @@ import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 
 import android.os.IBinder;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
